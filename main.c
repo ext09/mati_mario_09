eb695031da0443c905221ddf4980b159a100aacb
extz <extz@noot.(none)>
2009-10-06 22:54:01 +0400

    UP-TO-DATE Обновления....

--- main.c
+++ main.c
@@ -2,24 +2,65 @@
 #include <stdlib.h>
 #include <SDL/SDL.h>
 
+
+#define HEIGHT 10
+#define WIDTH 13
+
 void InitImages(void);
 void DrawIMG(SDL_Surface *img, int x, int y);
 void DrawIMG1(SDL_Surface *img, int x, int y, int w, int h, int sx, int sy);
-void DrawIMG2(SDL_Surface *img, int x, int y, int w, int h, int sx, int sy);
 void DrawBG(void);
 void DrawScene(void);
 
+
+int lev[HEIGHT][WIDTH];
+
+SDL_Surface *sky;
+SDL_Surface *earth;
+SDL_Surface *wood;
+SDL_Surface *screen;
 SDL_Surface *back;
-SDL_Surface *image;
+SDL_Surface *image1;
 SDL_Surface *box;
 SDL_Surface *screen;
 
-int xpos=0, ypos=200, xstep=0, ystep=0;
+int xpos=0, ypos=400, xstep=0, ystep=0;
 
+    void level()
+    {
+    // int lev[HEIGHT][WIDTH];
+    int  i,j;
+    FILE *fp;
+    if ((fp=fopen("lev.txt","r "))==NULL)
+    {
+        printf("Owubka co3danu9.\n");
+        exit(666);
+    }
+    for (i=0;i<HEIGHT;i++)
+    {
+        for (j=0;j<WIDTH;j++)
+        {
+            fscanf(fp,"%1d",&lev[i][j]);
+            //fprintf(fp, "%d",lev[i][j]);
+        }
+        fscanf(fp,"\n");
+    }
+    fclose(fp);
+    //*******************************************output
+    for (i=0;i<HEIGHT;i++)
+    {
+        for (j=0;j<WIDTH;j++)
+        {
+            printf("%2d",lev[i][j]);
+        }
+        putchar ('\n');
+    }
+    }
 /* ------------------------------------------- */
 int main(int argc, char *argv[])
 {
     Uint8* keys;
+    level();
 
     if ( SDL_Init(SDL_INIT_AUDIO|SDL_INIT_VIDEO) < 0 )
     {
@@ -29,8 +70,9 @@ int main(int argc, char *argv[])
 
     atexit(SDL_Quit);
 
-    SDL_WM_SetCaption(" qw ","qw");
+    SDL_WM_SetCaption("...","Mario");
     SDL_WM_SetIcon(SDL_LoadBMP("icon.bmp"), NULL);
+
 
     screen=SDL_SetVideoMode(640,480,32,SDL_HWSURFACE|SDL_DOUBLEBUF);
     if ( screen == NULL )
@@ -38,12 +80,10 @@ int main(int argc, char *argv[])
         printf("Unable to set 640x480 video: %s\n", SDL_GetError());
         exit(1);
     }
-
-
-    SDL_EnableKeyRepeat(1, SDL_DEFAULT_REPEAT_INTERVAL/5);
+    SDL_EnableKeyRepeat(1, SDL_DEFAULT_REPEAT_INTERVAL/6);
     InitImages();
-    SDL_SetColorKey(image, SDL_SRCCOLORKEY, SDL_MapRGB(image->format,255,255,255));
     DrawBG();
+    SDL_SetColorKey (image1, SDL_SRCCOLORKEY, SDL_MapRGB(image1->format,255,255,255));
     DrawScene();
     int done=0;
     while (done == 0)
@@ -62,11 +102,6 @@ int main(int argc, char *argv[])
                     done = 1;
                 }
                 keys = SDL_GetKeyState(NULL);
-
-                if (keys[SDLK_UP])
-                {
-                    ystep = -1;
-                }
                 if (keys[SDLK_DOWN])
                 {
                     ystep = 1;
@@ -91,13 +126,16 @@ int main(int argc, char *argv[])
     return 0;
 
 }
-
 /* ------------------------------------------- */
 void InitImages()
 {
     back=SDL_LoadBMP("bg.bmp");
-    image=SDL_LoadBMP("image.bmp");
+    image1=SDL_LoadBMP("image.bmp");
     box=SDL_LoadBMP("box.bmp");
+    sky=SDL_LoadBMP("sky.bmp");
+    wood=SDL_LoadBMP("wood.bmp");
+    earth=SDL_LoadBMP("earth.bmp");
+
 }
 
 /* ------------------------------------------- */
@@ -141,11 +179,38 @@ void DrawBG()
 /* ------------------------------------------- */
 void DrawScene()
 {
-    DrawIMG1(back, xpos, ypos, 135, 135, xpos, ypos);
+
+    int i,j,z2=0,y2=0;
+    for (i=0;i<HEIGHT;i++)
+    {
+        z2=0;
+        for (j=0;j<WIDTH;j++)
+        {
+            if (lev[i][j] == 0)
+            {
+                DrawIMG1(sky, z2, y2, 50, 50,0,0);
+                z2=z2+50;
+
+            }
+            if (lev[i][j] == 1)
+            {
+                DrawIMG1(earth, z2, y2, 50, 50,0,0);
+                z2=z2+50;
+            }
+            if (lev[i][j] == 2)
+            {
+                DrawIMG1(wood, z2, y2, 50, 50, 0,0);
+                z2=z2+50;
+            }
+        }
+        y2=y2+50;
+    }
+
+    DrawIMG1(sky, xpos, ypos, 52, 52, xpos, ypos);
     xpos += xstep;
     ypos += ystep;
-    DrawIMG(image, xpos, ypos);
+    DrawIMG(image1, xpos, ypos);
     xstep = ystep = 0;
-    DrawIMG1(box, 200, 200, 152, 152, 0, 0);
+//   DrawIMG1(box, 200, 340, 152, 152, 0, 0);
     SDL_Flip(screen);
 }
